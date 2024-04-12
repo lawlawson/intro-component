8e4971d47c85465d70181f3b74667455f52a8f25
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-04-12 18:44:31 +0100

    removed default error handling

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const form = document.querySelector('.signup-form form');
 const inputs = document.querySelectorAll('.signup-form form input');
 
-form.addEventListener('submit-button', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   inputs.forEach((input) => {
     if (!input.value) {
-      input.parentElement.classList.add('alert');
+      input.classList.add('alert');
     } else {
-      input.parentElement.classList.remove('alert');
-      // if (input.type == 'email') {
-      //   if (validateEmail(input.value)) {
-      //     input.parentElement.classList.remove('error');
-      //   } else {
-      //     input.parentElement.classList.add('error');
-      //   }
-      // }
+      input.classList.remove('alert');
+      if (input.type == 'email') {
+        if (validateEmail(input.value)) {
+          input.parentElement.classList.remove('alert');
+        } else {
+          input.parentElement.classList.add('alert');
+        }
+      }
     }
   });
 });
