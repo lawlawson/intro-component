91a6cc3cf5b2b576afb877527c753a36d26c4456
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-04-12 18:00:24 +0100

    error alert styling

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 const form = document.querySelector('.signup-form form');
 const inputs = document.querySelectorAll('.signup-form form input');
+
+form.addEventListener('submit-button', (e) => {
+  e.preventDefault();
+
+  inputs.forEach((input) => {
+    if (!input.value) {
+      input.parentElement.classList.add('alert');
+    } else {
+      input.parentElement.classList.remove('alert');
+      // if (input.type == 'email') {
+      //   if (validateEmail(input.value)) {
+      //     input.parentElement.classList.remove('error');
+      //   } else {
+      //     input.parentElement.classList.add('error');
+      //   }
+      // }
+    }
+  });
+});
+
+function validateEmail(email) {
+  var re =
+    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  return re.test(String(email).toLowerCase());
+}
